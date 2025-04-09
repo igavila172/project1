a0c781841be221609123f6163ac34e0815883fc2
Isaac Avilagit config --global user.email igavila172@gmail.com <isaacavila@Isaacs-MBP.lan>
2025-04-09 14:34:53 -0700

    Initial commit

---
+++ Monkey/Monkey/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) { // your code goes here
+        System.out.println("Shopping List:");
+        System.out.println("Apples\nBananas\nMilk\nEggs\nJuice\nAAA Batteries\nWine\nSpinach\nBread\nToothpaste");
+    }
+}
